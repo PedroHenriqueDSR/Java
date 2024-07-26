771df952e67f9503961b0f7c3d32c462aeb152c3
PedroHenriqueDSR <160232936+PedroHenriqueDSR@users.noreply.github.com>
2024-07-26 12:02:08 -0300

    grg

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert("oi, ja pode almossar :0")
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = 5;
+console.log('numeroSecreto')
+let chute = prompt('Escolha um número entre 1 e 69');
+
+// se chute for igual ao número secreto
+if (chute == numeroSecreto) {
+alert('Isso ai! Você descobriu o número secreto (24)');
+} else {
+alert('Você errou :(')
+}
