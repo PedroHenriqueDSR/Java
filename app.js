92949e22d61b353444b11969d6da00d2ffb5fa3e
PedroHenriqueDSR <160232936+PedroHenriqueDSR@users.noreply.github.com>
2024-07-26 12:09:14 -0300

    rg

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let chute = prompt('Escolha um número entre 1 e 69');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai! Você descobriu o número secreto (24)');
+alert('Isso ai gostosão, certinho :3 (24)');
 } else {
-alert('Você errou :(')
+alert('Você nao gosou :(')
 }
