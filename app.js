e4154f9cc851292b633f1bc22a4b9f5aeb0f165d
PedroHenriqueDSR <160232936+PedroHenriqueDSR@users.noreply.github.com>
2024-07-26 12:22:44 -0300

    fvdfvf

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-alert('oi gotosão');
+alert('oiinn');
 let numeroSecreto = 24;
 console.log('numeroSecreto')
 let chute = prompt('Escolha um número entre 1 e 69');
 
 // se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-alert('Isso ai gostosão, certinho :3 (24)');
+alert('hehe, certouu :3 (24)');
 } else {
-alert('Você nao gosou :(')
+alert('Vai toma no cu fdp, morra :(')
 }
